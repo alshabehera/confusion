7f3aa761bcc318282b10c8b6fbedd8272baaf36c
alsha <alshabehera7@gmail.com>
2023-03-26 08:57:42 +0530

    edit1

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">Welcome to React</h1>
         <p className="App-intro">
-          I LOVE YOU SWAPNEEL
+          THIS IS ALSHA BEHERA
         </p>
         <a
           className="App-link"
