19098e916cf5331aded73b9b7bee4b941d396219
alsha <alshabehera7@gmail.com>
2023-03-26 08:54:34 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className="App-title">Welcome to React</h1>
+        <p className="App-intro">
+          I LOVE YOU SWAPNEEL
         </p>
         <a
           className="App-link"
@@ -20,6 +24,7 @@ function App() {
       </header>
     </div>
   );
+}
 }
 
 export default App;
